652ebd862264bda18097eca4c59596f38a3efa0a
Quant-Kor <53992007+Quant-Kor@users.noreply.github.com>
2020-09-06 20:45:27 +0900

    origin

--- src/main/java/org/springframework/samples/petclinic/proxy/Cash.java
+++
@@ -1,8 +0,0 @@
-package org.springframework.samples.petclinic.proxy;
-
-public class Cash implements Payment{
-	@Override
-	public void pay(int amount) {
-		System.out.println(amount + " 현금 결제");
-	}
-}
